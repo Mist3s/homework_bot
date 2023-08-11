1da816ffa98b6d91a880ec1cabcf8bbc4aeeffac
andrey <a@a.ru>
2023-08-11 17:20:14 +0200

    up

--- homework.py
+++ homework.py
@@ -1,11 +1,17 @@
-...
+import os
+import time
 
-load_dotenv()
+import requests
+from telegram import Bot
+
+from dotenv import load_dotenv
+
+load_dotenv('.env')
 
 
-PRACTICUM_TOKEN = ...
-TELEGRAM_TOKEN = ...
-TELEGRAM_CHAT_ID = ...
+PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
+TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
@@ -20,24 +26,28 @@ HOMEWORK_VERDICTS = {
 
 
 def check_tokens():
-    ...
+    if (PRACTICUM_TOKEN or TELEGRAM_TOKEN or TELEGRAM_CHAT_ID) is None:
+        return False
 
 
 def send_message(bot, message):
-    ...
+    bot.send_message(TELEGRAM_CHAT_ID, message)
 
 
 def get_api_answer(timestamp):
-    ...
+    payload = {'from_date': timestamp}
+    response = requests.get(ENDPOINT, headers=HEADERS, params=payload)
+    return response.json()
 
 
 def check_response(response):
-    ...
+    if (response.get('homeworks') or response.get('current_date')) is None:
+        return False
 
 
 def parse_status(homework):
-    ...
-
+    homework_name = homework['homework_name']
+    verdict = HOMEWORK_VERDICTS[homework['status']]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -46,20 +56,20 @@ def main():
 
     ...
 
-    bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    timestamp = int(time.time())
-
-    ...
-
-    while True:
-        try:
-
-            ...
-
-        except Exception as error:
-            message = f'Сбой в работе программы: {error}'
-            ...
-        ...
+    # bot = Bot(token=TELEGRAM_TOKEN)
+    # timestamp = int(time.time())
+    #
+    # ...
+    #
+    # while True:
+    #     try:
+    #
+    #         ...
+    #
+    #     except Exception as error:
+    #         message = f'Сбой в работе программы: {error}'
+    #         ...
+    #     ...
 
 
 if __name__ == '__main__':
